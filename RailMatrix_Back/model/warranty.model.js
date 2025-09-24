fd19234ce966a6c416c204a7a0414319756f913c
Sufiyan Khan <sk7709972@gmail.com>
2025-09-24 07:52:34 +0530

    Initial commit with Frontend and Backend

--- RailMatrix_Back/model/warranty.model.js
+++ RailMatrix_Back/model/warranty.model.js
@@ -4,6 +4,7 @@ const WarrantySchema = new mongoose.Schema({
   component: { type: String, required: true },
   status: { type: String, enum: ["Under Review", "Approved", "Rejected"], default: "Under Review" },
   reportedBy: { type: String }
+
 });
 
 export default mongoose.model("Warranty", WarrantySchema);
